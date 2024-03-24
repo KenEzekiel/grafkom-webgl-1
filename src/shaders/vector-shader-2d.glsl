91222ae2de9a801e0c2c467058b453dea1c94e5f
arsaizdihar <arsadihar@gmail.com>
2024-03-24 19:49:09 +0700

    feat: change vector shader to enable rotation

--- src/shaders/vector-shader-2d.glsl
+++ src/shaders/vector-shader-2d.glsl
@@ -1,12 +1,22 @@
 // an attribute will receive data from a buffer
 attribute vec2 a_position;
 
+uniform vec2 u_rotation;
+uniform vec2 u_rotationPoint;
 uniform vec2 u_resolution;
 
 // all shaders have a main function
 void main() {
+  // to rotation point
+  vec2 position = a_position - u_rotationPoint;
+
+  // rotate
+  vec2 rotatedPosition = vec2(toRotationPoint.x * u_rotation.y + toRotationPoint.y * u_rotation.x, toRotationPoint.y * u_rotation.y - toRotationPoint.x * u_rotation.x);
+
+  position = rotatedPosition + u_rotationPoint;
+
   // convert the position from pixels to 0.0 to 1.0
-  vec2 zeroToOne = a_position / u_resolution;
+  vec2 zeroToOne = position / u_resolution;
 
   // convert from 0->1 to 0->2
   vec2 zeroToTwo = zeroToOne * 2.0;
