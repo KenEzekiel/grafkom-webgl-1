fa1f82e90102d993816cd7ddb646a88d511ac6fb
Kenneth Ezekiel <88850771+KenEzekiel@users.noreply.github.com>
2024-03-24 18:31:06 +0700

    feat: drawline

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,22 @@ import vectorShaderSource from "./shaders/vector-shader-2d.glsl";
 import "./style.css";
 
 function main() {
+  // Setup toolbar
   const toolbars = new Toolbars(["line", "square", "rectangle", "polygon"]);
+  // Setup canvas on html
   const canvasContainer =
     document.querySelector<HTMLDivElement>(".canvas-container")!;
   const canvas = document.querySelector<HTMLCanvasElement>("#main-canvas")!;
   canvas.width = canvasContainer.clientWidth;
   canvas.height = canvasContainer.clientHeight;
 
+  // Get WebGL
   const gl = canvas.getContext("webgl");
   if (!gl) {
     throw new Error("WebGL not supported");
   }
 
+  // Create shader function (abstraction)
   function createShader(
     gl: WebGLRenderingContext,
     type: number,
@@ -26,14 +30,16 @@ function main() {
     gl.shaderSource(shader, source);
     gl.compileShader(shader);
     const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    // If compile success, then return the shader
     if (success) {
       return shader;
     }
-
+    // If not, delete the shader
     console.log(gl.getShaderInfoLog(shader));
     gl.deleteShader(shader);
   }
 
+  // Create the vertex and fragment shader
   const vertexShader = createShader(gl, gl.VERTEX_SHADER, vectorShaderSource)!;
   const fragmentShader = createShader(
     gl,
@@ -41,6 +47,7 @@ function main() {
     fragmentShaderSource
   )!;
 
+  // Create program function (abstraction)
   function createProgram(
     gl: WebGLRenderingContext,
     vertexShader: WebGLShader,
@@ -50,15 +57,17 @@ function main() {
     gl.attachShader(program, vertexShader);
     gl.attachShader(program, fragmentShader);
     gl.linkProgram(program);
+    // If success, return the GL Program
     const success = gl.getProgramParameter(program, gl.LINK_STATUS);
     if (success) {
       return program;
     }
-
+    // If not, delete the GL program
     console.log(gl.getProgramInfoLog(program));
     gl.deleteProgram(program);
   }
 
+  // Create the program, and set the locations of attributes
   const program = createProgram(gl, vertexShader, fragmentShader)!;
   const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
   const resolutionUniformLocation = gl.getUniformLocation(
@@ -67,41 +76,94 @@ function main() {
   );
   const colorUniformLocation = gl.getUniformLocation(program, "u_color");
 
+  // Create and bind buffer
   const positionBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
+  // Create viewport
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
+  // Use the program with the attribute array and uniform 
   gl.useProgram(program);
   gl.enableVertexAttribArray(positionAttributeLocation);
   gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
 
   gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
 
+  // Setup the rectangles
   const rectangles: Array<
     [number, number, number, number, number, number, number]
   > = [];
 
-  setInterval(() => {
+  const points: Array<[number, number, number, number]> = [];
+
+  // Generate random rectangles
+  // setInterval(() => {
+  //   rectangles.push([
+  //     randomRange(0, gl.canvas.width),
+  //     randomRange(0, gl.canvas.height),
+  //     randomRange(10, 100),
+  //     randomRange(10, 100),
+  //     Math.random(),
+  //     Math.random(),
+  //     Math.random(),
+  //   ]);
+  //   draw(gl, rectangles, colorUniformLocation);
+  //   if (rectangles.length > 100) {
+  //     rectangles.shift();
+  //   }
+  // }, 200);
+  let pos: { x: any; y: any; }, clicked = false;
+  canvas.addEventListener("click", (e) => {
+    let {x, y} = getMousePosition(canvas, e);
+    
+    pos = getMousePosition(canvas, e);
+    clicked = true;
     rectangles.push([
-      randomRange(0, gl.canvas.width),
-      randomRange(0, gl.canvas.height),
-      randomRange(10, 100),
-      randomRange(10, 100),
-      Math.random(),
-      Math.random(),
-      Math.random(),
-    ]);
+          x,
+          canvas.height - y,
+          randomRange(10, 100),
+          randomRange(10, 100),
+          Math.random(),
+          Math.random(),
+          Math.random(),
+        ]);
+    drawLines(gl, points);
     draw(gl, rectangles, colorUniformLocation);
+    
     if (rectangles.length > 100) {
       rectangles.shift();
     }
-  }, 200);
+    
+    
+  })
+
+  
+
+  canvas.addEventListener("mousemove", (e) => {
+    let {x, y} = getMousePosition(canvas, e);
+
+    drawLines(gl, points);
+    draw(gl, rectangles, colorUniformLocation)
+    drawLine(gl, [pos.x,canvas.height - pos.y, x, canvas.height - y])
+    
+  })
 }
+
+function getMousePosition(canvas: HTMLCanvasElement, event: MouseEvent) {
+  let rect = canvas.getBoundingClientRect();
+  let x = event.clientX - rect.left;
+  let y = event.clientY - rect.top;
+  return { x, y };
+}
+
+
+
 function randomRange(min: number, max: number) {
   return Math.random() * (max - min) + min;
 }
 
+// Function to set buffer to a rectangle
 function setRectangle(
   gl: WebGLRenderingContext,
   x: number,
@@ -134,9 +196,37 @@ function draw(
       rectangle[4],
       rectangle[5],
       rectangle[6],
-      1
+      Math.random()
     );
     gl.drawArrays(gl.TRIANGLES, 0, 6);
+  });
+}
+
+function drawLine(
+  gl: WebGLRenderingContext,
+  points: [number, number, number, number]
+) {
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([points[0], points[1], points[2], points[3]]),
+    gl.STATIC_DRAW
+  );
+  gl.drawArrays(gl.LINES, 0, 2);
+}
+
+function drawLines(
+  gl: WebGLRenderingContext,
+  lines: Array<[number, number, number, number]>,
+) {
+  gl.clearColor(0, 0, 0, 1);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  lines.forEach((line) => {
+    gl.bufferData(
+      gl.ARRAY_BUFFER,
+      new Float32Array([line[0], line[1], line[2], line[3]]),
+      gl.STATIC_DRAW
+    );
+    gl.drawArrays(gl.LINES, 0, 2);
   });
 }
 
