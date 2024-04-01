ebf1de00408aa4fb82abaca4f0f9e7defc82eb4c
Fatih20 <fnri39@protonmail.com>
2024-04-01 21:20:30 +0700

    feat: modify colorpicker and slider

---
+++ src/lib/colorpicker.ts
@@ -0,0 +1,52 @@
+import { Color } from "./primitives";
+
+export class ColorPicker {
+  private colorpicker: HTMLInputElement;
+  public color: string;
+
+  constructor(id: string) {
+    this.colorpicker = document.querySelector(`#${id}`)!;
+    this.color = this.colorpicker.value;
+    this.colorpicker.addEventListener("input", () => {
+      this.color = this.colorpicker.value;
+    });
+  }
+
+  public onValueChange(callback: (value: string) => void) {
+    this.colorpicker.addEventListener("input", () => {
+      callback(this.colorpicker.value);
+    });
+  }
+
+  setColor(color: Color) {
+    this.color = this.rgbToHex(color[0], color[1], color[2]);
+    this.colorpicker.value = this.color;
+  }
+
+  private componentToHex(c: number) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+  }
+
+  private rgbToHex(r: number, g: number, b: number) {
+    return (
+      "#" +
+      this.componentToHex(r) +
+      this.componentToHex(g) +
+      this.componentToHex(b)
+    );
+  }
+
+  private hexToRgb(): Color {
+    const aRgbHex = this.color.slice(1).match(/.{1,2}/g)!;
+    return [
+      parseInt(aRgbHex[0], 16),
+      parseInt(aRgbHex[1], 16),
+      parseInt(aRgbHex[2], 16),
+    ];
+  }
+
+  public getColor(): Color {
+    return this.hexToRgb();
+  }
+}
--- src/lib/slider.ts
+++ src/lib/slider.ts
@@ -1,22 +1,17 @@
 export class Slider {
-  private onInput?: (value: number) => void;
   private slider: HTMLInputElement;
   private minimum: number;
   private maximum: number;
 
-  constructor(id: string, onInput?: (value: number) => void) {
+  constructor(id: string) {
     this.slider = document.querySelector(`#${id}`)!;
-    this.onInput = onInput;
     this.minimum = Number(this.slider.getAttribute("min"));
     this.maximum = Number(this.slider.getAttribute("max"));
+  }
 
-    console.log(this.minimum, this.maximum);
-
+  public onValueChange(callback: (value: number) => void) {
     this.slider.addEventListener("input", () => {
-      if (!this.onInput) {
-        return;
-      }
-      this.onInput(Number(this.slider.value));
+      callback(Number(this.slider.value));
     });
   }
 }
