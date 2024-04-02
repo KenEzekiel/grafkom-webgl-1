b83d1cb4e88ef8cdc09dbc936bd5fd4d34ba9b36
Kenneth Ezekiel <88850771+KenEzekiel@users.noreply.github.com>
2024-04-02 13:57:00 +0700

    fix: rectangle cannot be selected

--- src/lib/drawable/rectangle.ts
+++ src/lib/drawable/rectangle.ts
@@ -78,6 +78,6 @@ export class Rectangle extends Drawable {
     var x2 = x + width;
     var y1 = y;
     var y2 = y + height;
-    return [x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2];
+    return [x1, y1, x1, y2, x2, y1, x1, y2, x2, y2, x2, y1];
   }
 }
