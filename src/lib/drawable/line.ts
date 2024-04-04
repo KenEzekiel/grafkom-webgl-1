2c74761ec9185c2a5dcdde313daa51cc13c9faa6
Fatih20 <fnri39@protonmail.com>
2024-04-04 21:29:37 +0700

    fix: isSelected bug in line

--- src/lib/drawable/line.ts
+++ src/lib/drawable/line.ts
@@ -4,12 +4,16 @@ import { Drawable } from "./base";
 
 export class Line extends Drawable {
   public type = "line";
+  public length: number;
   constructor(
     public points: [Point, Point],
     color: Color,
     program: ApplicationProgram
   ) {
     super(color, program);
+    this.length = Math.sqrt(
+      (points[0].x - points[1].x) ** 2 + (points[0].y - points[1].y) ** 2
+    );
   }
 
   public proximityThickness = 5;
@@ -25,6 +29,13 @@ export class Line extends Drawable {
     };
   }
 
+  public calculateLength() {
+    this.length = Math.sqrt(
+      (this.points[0].x - this.points[1].x) ** 2 +
+        (this.points[0].y - this.points[1].y) ** 2
+    );
+  }
+
   translate({ x, y }: Point): void {
     this.points[0].x += x;
     this.points[0].y += y;
@@ -34,14 +45,61 @@ export class Line extends Drawable {
   }
 
   isSelected(mousePosition: Point): boolean {
+    // This code block prevents line distance calculation being done as if the line extends infinitely long by calculating the angles of the triangle made by the line's point 0, the line's point 1, and the position of the mouse
+    {
+      const distanceToPoint0 = Math.sqrt(
+        (mousePosition.x - this.points[0].x) ** 2 +
+          (mousePosition.y - this.points[0].y) ** 2
+      );
+
+      const distanceToPoint1 = Math.sqrt(
+        (mousePosition.x - this.points[1].x) ** 2 +
+          (mousePosition.y - this.points[1].y) ** 2
+      );
+
+      const cosAtPoint0 =
+        (this.length ** 2 + distanceToPoint0 ** 2 - distanceToPoint1 ** 2) /
+        (2 * this.length * distanceToPoint0);
+
+      const cosAtPoint1 =
+        (distanceToPoint0 ** 2 + distanceToPoint1 ** 2 - this.length ** 2) /
+        (2 * this.length * distanceToPoint1);
+
+      console.table({
+        distanceToPoint0,
+        distanceToPoint1,
+        length: this.length,
+        cosAtPoint0,
+        cosAtPoint1,
+      });
+
+      // The mouse is located outside the infinitely long rectangular shape perpendicular to the line
+      if (
+        !(cosAtPoint0 < 0 || cosAtPoint1 < 0) ||
+        cosAtPoint0 > 1 ||
+        cosAtPoint1 > 1
+      ) {
+        return false;
+      }
+    }
+
+    // Normal line-point distance calculation
     const dx = this.points[0].x - this.points[1].x;
     const dy = this.points[0].y - this.points[1].y;
 
-    const dxo = this.points[0].x - mousePosition.x;
-    const dyo = this.points[0].y - mousePosition.y;
+    const dxo = this.points[1].x - mousePosition.x;
+    const dyo = this.points[1].y - mousePosition.y;
 
     const distance =
       Math.abs(dx * dyo - dy * dxo) / Math.sqrt(dx ** 2 + dy ** 2);
+
+    console.table({
+      dx,
+      dy,
+      dxo,
+      dyo,
+      distance,
+    });
 
     return distance <= this.proximityThickness;
   }
@@ -68,10 +126,12 @@ export class Line extends Drawable {
     this.points[this.selectedVertexIdx].x = beforeLoc.x + translation.x;
     this.points[this.selectedVertexIdx].y = beforeLoc.y + translation.y;
     this.resetPointsCache();
+    this.calculateLength();
   }
 
   moveDrawing(point: Point): void {
     this.points[1] = point;
+    this.calculateLength();
     this.resetPointsCache();
   }
 
