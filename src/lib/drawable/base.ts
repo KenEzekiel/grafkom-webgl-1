521614ac948766ffd19c49c126250f560c4a9bf0
Fatih20 <fnri39@protonmail.com>
2024-04-04 19:02:38 +0700

    feat: add model

--- src/lib/drawable/base.ts
+++ src/lib/drawable/base.ts
@@ -1,7 +1,7 @@
 import type { ApplicationProgram } from "../../application";
 import { Color, Point, Size, Vec2, rotatePoint } from "../primitives";
 export abstract class Drawable {
-  protected rotation: Vec2 = [0, 1];
+  public rotation: Vec2 = [0, 1];
   public rotationDegree: number = 0;
   public localRotatedDegree: number = 0;
   public selectedVertex: Point | undefined;
